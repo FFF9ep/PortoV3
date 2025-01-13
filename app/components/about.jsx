582ecd3be2eee2c542e1ccc36661f2512db6ff98
fandi a <fandiiardy@gmail.com>
2025-01-13 21:30:05 +0700

    update

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const about = () => {
   return (
     <div id='about' className='w=full px-[12%] py-10 scroll-mt-20'>
-        <h4 className=''>Introduction</h4>
-        <h2>About Me</h2>
+      <h4 className='w-14'>Introduction</h4>
+      <h2>About Me</h2>
     </div>
   )
 }
