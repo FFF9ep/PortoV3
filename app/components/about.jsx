385f8418f3526f4a9ef4a626d41da21ffcbc7552
fandi a <fandiiardy@gmail.com>
2025-01-14 18:59:59 +0700

    update

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -4,7 +4,7 @@ const about = () => {
   return (
     <div id='about' className='w=full px-[12%] py-10 scroll-mt-20'>
       <h4 className='w-14'>Introduction</h4>
-      <h2>About Me</h2>
+      <h2 className='w-14'>About Me</h2>
     </div>
   )
 }
