d10bc7066f422ee75168dc291bb516aedee4d448
fandi a <fandiiardy@gmail.com>
2025-01-16 16:32:43 +0700

    update service

--- app/components/services.jsx
+++ app/components/services.jsx
@@ -1,3 +1,5 @@
+import { assets, serviceData } from '@/assets/assets'
+import Image from 'next/image'
 import React from 'react'
 
 const services = () => {
@@ -7,6 +9,23 @@ const services = () => {
         <h2 className='text-center text-5xl font-Ovo'>My Services</h2>
 
         <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>I am Fullstack Web Developer from Indonesia, with 1 year of experience in multiple programming language </p>
+
+        <div className='grid grid-cols-4 gap-6 my-10'>
+            {serviceData.map(({icon, title, description, link}, index) => (
+                <div 
+                className='border border-gray-400 rounded-lg px-8 py-12 hover:shadow-black cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500'
+                key={index}>
+                    <Image src={icon} alt='' className='w-10' />
+                    <h3 className='text-lg my-4 text-gray-700'>{title}</h3>
+                    <p className='text-sm text-gray-600 leading-5'>
+                        {description}
+                    </p>
+                    <a href={link} className='flex items-center gap-2 text-sm mt-5'>
+                        Read More <Image alt='' src={assets.right_arrow} className='w-4'/>
+                    </a>
+                </div>
+            ))}
+        </div>
     </div>
   )
 }
